40572d7fd840e8099475a5811eafb5f75ba4b480
Eddie <edward.torpy@my.jcu.edu.au>
2017-05-07 18:26:28 +1000

    Bubble animates

--- app/src/main/java/com/edwardtorpy/bubbleanimation/MainActivity.java
+++ app/src/main/java/com/edwardtorpy/bubbleanimation/MainActivity.java
@@ -3,11 +3,16 @@ package com.edwardtorpy.bubbleanimation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     public void bubble(View view) {
 
+        ImageView bubble = (ImageView) findViewById(R.id.bubbleimageView);
+        bubble.setTranslationY(400);
+        bubble.setAlpha(1f);
+        bubble.animate().translationYBy(-450).setDuration(1000);
 
     }
 
