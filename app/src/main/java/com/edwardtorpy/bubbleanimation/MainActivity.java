779ce27914a87c575eb6b423e3a37b49363d9297
Eddie <edward.torpy@my.jcu.edu.au>
2017-05-08 20:45:47 +1000

    Randomly  changes bubble position on each click

--- app/src/main/java/com/edwardtorpy/bubbleanimation/MainActivity.java
+++ app/src/main/java/com/edwardtorpy/bubbleanimation/MainActivity.java
@@ -4,13 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     public void bubble(View view) {
 
+        Random rand = new Random();
+        int xDisplacement = rand.nextInt(100);
+        xDisplacement -= 50;
+        
         ImageView bubble = (ImageView) findViewById(R.id.bubbleimageView);
         bubble.setTranslationY(400);
+        bubble.setTranslationX(xDisplacement * 5);
         bubble.setAlpha(1f);
         bubble.animate().translationYBy(-450).setDuration(1000);
 
